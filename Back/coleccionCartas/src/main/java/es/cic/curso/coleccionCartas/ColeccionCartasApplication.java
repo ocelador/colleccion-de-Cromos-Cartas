e0b45ceebe5899cef0fa6787117d0bd968d9bdc7
PabloPDV <you@example.com>
2024-09-02 08:42:31 +0200

    Subida esquema proyecto back

---
+++ Back/coleccionCartas/src/main/java/es/cic/curso/coleccionCartas/ColeccionCartasApplication.java
@@ -0,0 +1,13 @@
+package es.cic.curso.coleccionCartas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColeccionCartasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColeccionCartasApplication.class, args);
+	}
+
+}
