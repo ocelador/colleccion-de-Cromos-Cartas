87110963e1b481ebe605b82ac9c8a66f6c09e398
Curso003 <correo@correo.co>
2024-09-04 11:25:53 +0200

    correccion menor base de datos

--- Back/coleccionCartas/src/main/resources/data.sql
+++ Back/coleccionCartas/src/main/resources/data.sql
@@ -6,7 +6,6 @@ INSERT INTO usuario (nombre, email, password) VALUES ('Maria Lopez', 'maria.lope
 INSERT INTO album (nombre, descripcion, usuario_id, imagen) VALUES ('La Liga', 'Álbum de cromos de la liga española de fútbol 2024-25', 1, '/home/curso003/Desktop/colleccion-de-Cromos-Cartas/Back/coleccionCartas/images/liga.png');
 INSERT INTO album (nombre, descripcion, usuario_id, imagen) VALUES ('YU-GI-OH', 'Álbum de cartas de la conocida serie de animacion YU-GI-OH', 2, '/home/curso003/Desktop/colleccion-de-Cromos-Cartas/Back/coleccionCartas/images/yugioh.jpg');
 
-
 -- Insertar datos en la tabla cromos
 INSERT INTO cromos (nombre, descripcion, anio, valor, rareza, album_id) VALUES ('Federico Valverde', 'Futbolista uruguayo nacionalizado español en 2020 que juega como centrocampista actualmente en el Real Madrid C. F.', 2023, 10.5, 'Rara', 1);
 INSERT INTO cromos (nombre, descripcion, anio, valor, rareza, album_id) VALUES ('Kylian Mbappé', 'Futbolista francés que juega como delantero en el Real Madrid C. F. de la Primera División de España. Es internacional con la selección de Francia', 2023, 5.0, 'Muy Rara', 1);
