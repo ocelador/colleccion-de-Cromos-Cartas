f39d2f8d0e7ecf9a070696d60e76db62152cc32d
Curso003 <correo@correo.co>
2024-09-06 08:30:09 +0200

    cambios en los test de cyrpess

--- Front/cypress/integration/albumes.spec.js
+++ Front/cypress/integration/albumes.spec.js
@@ -1,16 +1,38 @@
-describe('Albumes Page', () => {
-    it('should display the list of albums', () => {
-      cy.visit('/albumes');
-      cy.contains('h1', 'Gestión de Álbumes');
-      cy.get('.list-group-item').should('have.length.greaterThan', 0);
-    });
-  
-    it('should create a new album', () => {
-      cy.visit('/albumes');
-      cy.get('#nombre', { timeout: 10000 }).should('be.visible').type('Nuevo Álbum'); // Aumentar el tiempo de espera a 10 segundos
-      cy.get('#descripcion').type('Descripción del nuevo álbum');
-      cy.get('#file').attachFile('/home/curso003/Desktop/colleccion-de-Cromos-Cartas/Back/coleccionCartas/images/pokemon.jpg'); // Asegúrate de tener un archivo de imagen en la ruta especificada
-      cy.get('.btn-primary').click();
-      cy.get('.list-group-item').should('contain', 'Nuevo Álbum');
-    });
+describe('Albums Page', () => {
+  beforeEach(() => {
+    cy.visit('/albums');
   });
+
+  it('should display the list of albums', () => {
+    cy.url().should('include', '/albums');
+    cy.get('h1').should('be.visible').and('contain', 'Gestión de Álbumes');
+    cy.get('.list-group-item').should('have.length.greaterThan', 0);
+  });
+
+  it('should create a new album', () => {
+    cy.get('#nombre').should('be.visible').type('Nuevo Álbum');
+    cy.get('#descripcion').type('Descripción del nuevo álbum');
+    cy.get('#file').attachFile('path/to/your/image.jpg'); // Asegúrate de tener un archivo de imagen en la ruta especificada
+    cy.get('.btn-primary').click();
+    cy.get('.list-group-item').should('contain', 'Nuevo Álbum');
+  });
+
+  it('should update an existing album', () => {
+    cy.get('.list-group-item').first().within(() => {
+      cy.get('.btn-info').click(); // Seleccionar el primer álbum
+    });
+    cy.get('#nombre').clear().type('Álbum Actualizado');
+    cy.get('#descripcion').clear().type('Descripción actualizada');
+    cy.get('.btn-warning').click(); // Hacer clic en el botón "Actualizar Álbum"
+    cy.get('.list-group-item').should('contain', 'Álbum Actualizado');
+  });
+
+  it('should delete an existing album', () => {
+    cy.get('.list-group-item').first().within(() => {
+      cy.get('.btn-danger').click(); // Hacer clic en el botón "Eliminar"
+    });
+    cy.get('.modal-container').should('be.visible');
+    cy.get('.modal-footer .btn-primary').click(); // Confirmar eliminación
+    cy.get('.list-group-item').should('not.contain', 'Álbum Actualizado');
+  });
+});
--- Front/cypress/integration/cromos.spec.js
+++ Front/cypress/integration/cromos.spec.js
@@ -1,18 +1,43 @@
 describe('Cromos Page', () => {
-    it('should display the list of cromos', () => {
-      cy.visit('/cromos');
-      cy.get('h1', { timeout: 10000 }).should('be.visible').and('contain', 'Gestión de Cromos'); // Aumentar el tiempo de espera a 10 segundos
-      cy.get('.list-group-item').should('have.length.greaterThan', 0);
-    });
-  
-    it('should create a new cromo', () => {
-      cy.visit('/cromos');
-      cy.get('#nombre', { timeout: 10000 }).should('be.visible').type('Nuevo Cromo'); // Aumentar el tiempo de espera a 10 segundos
-      cy.get('#descripcion').type('Descripción del nuevo cromo');
-      cy.get('#anio').type('2023');
-      cy.get('#valor').type('100');
-      cy.get('#rareza').type('Raro');
-      cy.get('.btn-primary').click();
-      cy.get('.list-group-item').should('contain', 'Nuevo Cromo');
-    });
+  beforeEach(() => {
+    cy.visit('/cromos');
   });
+
+  it('should display the list of cromos', () => {
+    cy.url().should('include', '/cromos');
+    cy.get('h1').should('be.visible').and('contain', 'Gestión de Cromos');
+    cy.get('.list-group-item').should('have.length.greaterThan', 0);
+  });
+
+  it('should create a new cromo', () => {
+    cy.get('#nombre').should('be.visible').type('Nuevo Cromo');
+    cy.get('#descripcion').type('Descripción del nuevo cromo');
+    cy.get('#anio').type('2023');
+    cy.get('#valor').type('100');
+    cy.get('#rareza').type('Raro');
+    cy.get('.btn-primary').click();
+    cy.get('.list-group-item').should('contain', 'Nuevo Cromo');
+  });
+
+  it('should update an existing cromo', () => {
+    cy.get('.list-group-item').first().within(() => {
+      cy.get('.btn-success').click(); // Seleccionar el primer cromo
+    });
+    cy.get('#nombre').clear().type('Cromo Actualizado');
+    cy.get('#descripcion').clear().type('Descripción actualizada');
+    cy.get('#anio').clear().type('2024');
+    cy.get('#valor').clear().type('200');
+    cy.get('#rareza').clear().type('Muy Raro');
+    cy.get('.btn-warning').click(); // Hacer clic en el botón "Actualizar Cromo"
+    cy.get('.list-group-item').should('contain', 'Cromo Actualizado');
+  });
+
+  it('should delete an existing cromo', () => {
+    cy.get('.list-group-item').first().within(() => {
+      cy.get('.btn-danger').click(); // Hacer clic en el botón "Eliminar"
+    });
+    cy.get('.modal-container').should('be.visible');
+    cy.get('.modal-footer .btn-primary').click(); // Confirmar eliminación
+    cy.get('.list-group-item').should('not.contain', 'Cromo Actualizado');
+  });
+});
